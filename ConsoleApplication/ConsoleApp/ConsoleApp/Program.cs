d8bafe89164f12e7b65ee9c324d1c96d47386299
Oleh Kit <kitoleg167@gmail.com>
2025-10-13 23:50:58 +0300

    Create Lectures

--- ConsoleApplication/ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApplication/ConsoleApp/ConsoleApp/Program.cs
@@ -1,1 +1,13 @@
 ﻿using System;
+using ConsoleApp.models;
+
+namespace ConsoleApp;
+
+class Program
+{
+    public static void Main()
+    {
+        var lectureTest = new Lecture();
+        lectureTest.AddLecture("Oleh", "Kit");
+    }
+}
