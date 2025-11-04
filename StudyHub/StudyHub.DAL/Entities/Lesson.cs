6d187256ee79b3a33cd1f91572201091466af379
Oleh Kit <kitoleg167@gmail.com>
2025-11-04 08:17:21 +0200

    UTS8

--- StudyHub/StudyHub.DAL/Entities/Lesson.cs
+++ StudyHub/StudyHub.DAL/Entities/Lesson.cs
@@ -7,10 +7,9 @@ public class Lesson
     public LessonSlots LessonSlot { get; set; }
     public Subject Subject { get; set; }
     public List<Schedule> Schedules { get; set; }
-
-    // додав в цю сутність три нові необхідні для парсера поля, їх немає в UML діаграмі, тому треба буде додати
-    public string Type { get; set; } // лекція / лаб. / практ.
-    public string Room { get; set; } // аудиторія
-    public string Day { get; set; }  // день тижня
+    
+    public string Type { get; set; } 
+    public string Room { get; set; }
+    public string Day { get; set; } 
 
 }
